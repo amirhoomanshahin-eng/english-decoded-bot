c47c763177a7cf9a209ea747850769b212a45265
amirhoomanshahin-eng <amir.hooman.shahin@gmail.com>
2025-09-19 15:17:13 +0330

    Update app.js

--- public/app.js
+++ public/app.js
@@ -13,14 +13,3 @@ if (user) {
 } else {
   welcomeEl.innerText = "Hello, guest!";
 }
-
-// Button event - fetch backend API
-document.getElementById("fetchBtn").addEventListener("click", async () => {
-  try {
-    const res = await fetch("/api/hello");
-    const data = await res.json();
-    document.getElementById("response").innerText = data.message;
-  } catch (err) {
-    document.getElementById("response").innerText = "Error: " + err.message;
-  }
-});
