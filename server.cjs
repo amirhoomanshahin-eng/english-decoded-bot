ebc9af121aa3ed541b713b59f6da02948a445e51
amirhoomanshahin-eng <225597403+amirhoomanshahin-eng@users.noreply.github.com>
2025-11-24 13:30:29 +0330

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1,7 +1,7 @@
    require("dotenv").config();
 const express = require("express");
 const path = require("path");
-import fetch = from("node-fetch");
+import fetch = from 'node-fetch';
 const crypto = require("crypto");        // <--- NEW
 const { exec } = require("child_process"); // <--- NEW
 
